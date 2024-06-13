c6cb55603bf17bc641f581be47d66f07b7ac748d
Dominic <dominic.lee876@gmail.com>
2024-06-13 09:04:11 -0400

    pre commit hooks work

--- src/Counter.sol
+++ src/Counter.sol
@@ -2,11 +2,11 @@
 pragma solidity ^0.8.13;
 
 contract Counter {
-    uint256 public number;
-
     function setNumber(uint256 newNumber) public {
         number = newNumber;
     }
+
+    uint256 public number;
 
     function increment() public {
         number++;
